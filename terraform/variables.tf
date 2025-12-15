934df513ffe294f10fa4c1ccd82ac4711ce1876c
Marvin Korir <marvinkorir.mk@gmail.com>
2025-12-15 16:51:02 +1100

    added terraform config for deploying to ec2

---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable vpc_cidr_block{
+    default = "10.0.0.0/16"
+}
+variable subnet_cidr_block{
+    default = "10.0.0.0/24"
+}
+variable env_prefix {
+    default = "dev"
+}
+variable avail_zone {
+    default = "ap-southeast-2"
+}
+variable "my_ip" {
+    default = "106.71.63.97"
+}
+
+variable "jenkins_ip" {
+  default = "209.38.26.183/32"
+}
+variable "instance_type" {
+    default = "t2.micro"
+}
